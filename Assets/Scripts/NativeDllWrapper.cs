5540966f1dbe9139bbbd6ec34c461e44e71ef57d
Sinan Sakaoglu <sinanssakaoglu@gmail.com>
2018-12-21 04:59:34 +0200

    Initial commit

---
+++ Assets/Scripts/NativeDllWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class NativeDllWrapper : MonoBehaviour
+{
+    
+    [DllImport("Dll1")]
+    public static extern int NthFibonacciNumber(int n);
+
+    [DllImport("Dll1", CharSet = CharSet.Ansi)]
+    private static extern IntPtr DeletePattern(string target, string pattern, int targetLenght, int patternLenght, char replacementChar);
+
+    [DllImport("Dll1")]
+    private static extern void SortCubeArray(Cube[] cubes, int cubeCount);
+
+    public static void SortTheCubes(Cube[] cubes)
+    {
+        // Do inplace sort for cubes array
+        SortCubeArray(cubes, cubes.Length);
+    }
+
+    public static string FindAndCensor(string targetString, string pattern, char sensorChar)
+    {
+        
+        IntPtr stringPtr = DeletePattern(targetString, pattern, targetString.Length, pattern.Length, sensorChar);
+
+        string censoredSentence = Marshal.PtrToStringAnsi(stringPtr);
+
+        return censoredSentence;
+
+    }
+
+}
+
+public struct Cube
+{
+    public int y, index;
+}
---
+++ Assets/Scripts/TestDLLScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestDLLScript : MonoBehaviour
+{
+
+    public Text fibonacciResultText;
+    public Text censorResultText;
+
+    public void ArrangeCubes()
+    {
+        // Get all childeren of the object that this script is attached to
+        Transform[] ts = GetComponentsInChildren<Transform>();
+
+        Cube[] cubes = new Cube[ts.Length - 1];
+
+        //first element is the cubeHolder, which we ignore
+        for (int i = 1; i <= cubes.Length; i++)
+        {
+            // record the y coord and the current index of each cube to the cubes array
+            cubes[i - 1] = new Cube
+            {
+                y = (int)ts[i].position.y,
+                index = i - 1
+            };
+
+        }
+
+        NativeDllWrapper.SortTheCubes(cubes);
+
+        for (int i = 0; i < cubes.Length; i++)
+        {
+            // assign the correct location to each cube
+            // +1 is there to avoid touching cubeHolder object which is ts[0]
+            Vector3 newPosition = ts[cubes[i].index + 1].position;
+
+            newPosition.z = i;
+
+            ts[cubes[i].index + 1].position = newPosition;
+
+        }
+
+    }
+    public void DemoOthers()
+    {
+        fibonacciResultText.text = NativeDllWrapper.NthFibonacciNumber(23).ToString();
+
+        string pattern = "damn";
+        char sensorChar = '*';
+
+        censorResultText.text = NativeDllWrapper.FindAndCensor(censorResultText.text, pattern, sensorChar);
+
+
+
+
+    }
+    /*private void Start()
+    {
+        string target = "This is a damn sentence.";
+        string pattern = "damn";
+
+        Debug.Log("Target sentence: \"" + target + "\"");
+
+        Debug.Log("Censored sentence: \"" + NativeDllWrapper.FindAndCensor(target,pattern,'*') + "\"");
+
+        Debug.Log("23rd term of Fibonacci is: " + NativeDllWrapper.NthFibonacciNumber(23));
+    }*/
+
+
+
+}
